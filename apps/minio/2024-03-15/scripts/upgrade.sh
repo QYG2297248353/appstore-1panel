f828276fe6e89ce859d86808538f327c712d42ce
萌森 <qyg2297248353@163.com>
2024-03-22 13:22:40 +0800

    升级版本 Minio 2024-03-15

--- apps/minio/2024-03-15/scripts/upgrade.sh
+++ apps/minio/2024-03-15/scripts/upgrade.sh
@@ -9,6 +9,18 @@ if [[ -f ./.env ]]; then
     echo 'MINIO_IMAGE="minio/minio:RELEASE.2024-03-15T01-07-19Z"' >> ./.env
   fi
 
+  if grep -q "MINIO_BROWSER_REDIRECT_URL" ./.env; then
+    echo "MINIO_BROWSER_REDIRECT_URL 已存在"
+  else
+    echo 'MINIO_BROWSER_REDIRECT_URL="http://127.0.0.1:9001"' >> ./.env
+  fi
+
+  if grep -q "MINIO_SERVER_URL" ./.env; then
+    echo "MINIO_SERVER_URL 已存在"
+  else
+    echo 'MINIO_SERVER_URL="http://127.0.0.1:9000"' >> ./.env
+  fi
+
   echo "Check Finish."
 
 else
