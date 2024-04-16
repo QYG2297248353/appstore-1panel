81d1287b1a4905a2253feaa454f584748766804e
萌森 <qyg2297248353@163.com>
2024-04-16 10:25:07 +0800

    优化Immich升级脚本

--- apps/immich/1.101.0-db/scripts/init.sh
+++ apps/immich/1.101.0-db/scripts/init.sh
@@ -3,12 +3,6 @@
 if [ -f .env ]; then
   source .env
 
-  if [ -n "$(docker network ls -q -f name=$IMMICH_NETWORK)" ]; then
-    docker network rm $IMMICH_NETWORK
-  fi
-
-  sed -i "s/\${IMMICH_NETWORK}/$IMMICH_NETWORK/" docker-compose.yml
-
   echo "Directories and permissions set successfully."
 
 else
--- apps/immich/1.101.0-db/scripts/upgrade.sh
+++ apps/immich/1.101.0-db/scripts/upgrade.sh
@@ -3,19 +3,6 @@
 if [ -f .env ]; then
   source .env
 
-  if grep -q "IMMICH_NETWORK" ./.env; then
-    echo "IMMICH_NETWORK found."
-    sed -i 's/IMMICH_NETWORK=.*/IMMICH_NETWORK="immich-net"/' ./.env
-  else
-    echo 'IMMICH_NETWORK="immich-net"' >>./.env
-  fi
-
-  if [ -n "$(docker network ls -q -f name=$IMMICH_NETWORK)" ]; then
-    docker network rm $IMMICH_NETWORK
-  fi
-
-  sed -i "s/\${IMMICH_NETWORK}/$IMMICH_NETWORK/" docker-compose.yml
-
   echo "Directories and permissions set successfully."
 
 else
