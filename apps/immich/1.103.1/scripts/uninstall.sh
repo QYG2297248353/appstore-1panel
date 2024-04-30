332a4ff3f1790cc01716c6c390653d19df8f4d63
萌森 <qyg2297248353@163.com>
2024-04-30 15:28:59 +0800

    调试 Immich

--- apps/immich/1.103.1/scripts/uninstall.sh
+++ apps/immich/1.103.1/scripts/uninstall.sh
@@ -3,15 +3,6 @@
 if [ -f .env ]; then
   source .env
 
-  # 卸载容器名称以 immich- 开头的容器
-  docker ps -a | grep immich- | awk '{print $1}' | xargs docker rm -f
-
-  # 删除容器名称以 immich- 开头的镜像
-  docker images | grep immich- | awk '{print $3}' | xargs docker rmi -f
-
-  # 删除 immich- 开头的网络
-  docker network ls | grep immich- | awk '{print $1}' | xargs docker network rm
-
   echo "Check Finish."
 
 else
