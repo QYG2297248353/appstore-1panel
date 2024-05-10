b160464127b718c7d431c7dc62fe8433eedadd1d
萌森 <qyg2297248353@163.com>
2024-05-10 13:52:17 +0800

    发布应用 NeZha 哪吒监控 0.16.25

--- apps/nezha/0.16.25/scripts/init.sh
+++ apps/nezha/0.16.25/scripts/init.sh
@@ -6,7 +6,7 @@ if [[ -f ./.env ]]; then
   mkdir -p $NEZHA_DASHBOARD_ROOT_PATH
   mkdir -p $NEZHA_DASHBOARD_ROOT_PATH/data
 
-  cp ./data/config.yaml ./data/nezha-config.yaml
+  cp -f ./data/config.yaml ./data/nezha-config.yaml
   sed -i "s/grpcport: nz_grpc_port/grpcport: $PANEL_APP_PORT_GRPC/g" ./data/nezha-config.yaml
   sed -i "s/type: \"nz_oauth2_type\"/type: \"$OAUTH2_TYPE\"/g" ./data/nezha-config.yaml
   sed -i "s/admin: \"nz_admin_logins\"/admin: \"$OAUTH2_ADMIN\"/g" ./data/nezha-config.yaml
@@ -16,7 +16,7 @@ if [[ -f ./.env ]]; then
   sed -i "s/brand: \"nz_site_title\"/brand: \"$NZ_SITE_TITLE\"/g" ./data/nezha-config.yaml
   sed -i "s/cookiename: \"nezha-dashboard\"/cookiename: \"$NZ_COOKIE_NAME\"/g" ./data/nezha-config.yaml
   sed -i "s/theme: \"default\"/theme: \"$NZ_THEME\"/g" ./data/nezha-config.yaml
-  cp ./data/nezha-config.yaml $NEZHA_DASHBOARD_ROOT_PATH/data/config.yaml
+  cp -f ./data/nezha-config.yaml $NEZHA_DASHBOARD_ROOT_PATH/data/config.yaml
   chmod -R 777 $NEZHA_DASHBOARD_ROOT_PATH
 
   echo "Check Finish."
