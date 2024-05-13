31ae621f66076de451d79a475e60b1e51d8d368e
Meng Sen <qyg2297248353@gmail.com>
2024-05-13 12:23:57 +0800

    Update renovate-app-version.sh

--- .github/workflows/renovate-app-version.sh
+++ .github/workflows/renovate-app-version.sh
@@ -20,7 +20,7 @@ do
 
 	  # Trim the "v" prefix
 	  trimmed_version=${version/#"v"}
-
-      mv apps/$app_name/$old_version apps/$app_name/$trimmed_version
+      mkdir -p apps/$app_name/$trimmed_version
+      cp -R apps/$app_name/$old_version/* apps/$app_name/$trimmed_version
     fi
 done
