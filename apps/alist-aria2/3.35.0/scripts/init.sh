91ff71e56aced6f183bfe335e976dd76b8d4e796
萌森 <qyg2297248353@163.com>
2024-07-11 17:42:26 +0800

    测试新版升级脚本-解决 env_file 字段问题

--- apps/alist-aria2/3.35.0/scripts/init.sh
+++ apps/alist-aria2/3.35.0/scripts/init.sh
@@ -3,13 +3,6 @@
 if [ -f .env ]; then
   source ./.env
 
-  if [ -f custom.env ]; then
-    echo -e "\n# Start of .env content\n" >> custom.env
-    cat .env >> custom.env
-  else
-    echo "Error: custom.env file not found."
-  fi
-
   echo "Check Finish."
 
 else
