b5887cabc3ddab7b9a672c8abae2bfea1b965a06
萌森 <qyg2297248353@163.com>
2024-07-16 13:42:27 +0800

    维护 gitea outline

--- apps/outline/0.77.3/scripts/init.sh
+++ apps/outline/0.77.3/scripts/init.sh
@@ -3,8 +3,11 @@
 if [ -f .env ]; then
   source .env
 
-  # setup-1
+  # setup-1 add default values
   echo "ENV_FILE=.env" >> .env
+
+  # setup-2 remove empty values
+  sed -i '/^.*=""/d' .env
 
   echo "Check Finish."
 
